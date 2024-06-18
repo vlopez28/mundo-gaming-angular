03e1693cb7567ade1163e47e2b76b128ae8a3120
Victoria Lopez <vlopez@alumnos.exa.unicen.edu.ar>
2024-06-18 19:04:56 -0300

    cambio de url

--- src/app/game-data.service.ts
+++ src/app/game-data.service.ts
@@ -2,13 +2,15 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, tap, throwError } from 'rxjs';
 import { Game } from './game-list/Game'
-const URL = 'https://apimocha.com/mundogaming/api/games';
-
+//const URL = 'https://apimocha.com/mundogaming/api/games';
+const URL = 'https://api.mockfly.dev/mocks/78ee371a-2383-4426-9611-e05c5b026eef/games';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameDataService {
+
+
 
   constructor(private http: HttpClient) { }
   //consumir api
